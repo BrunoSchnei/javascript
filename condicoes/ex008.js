4b6a03dd7910997b38a9d07409f32f3f0a8dff12
Bruno Schneider <84298233+BrunoSchnei@users.noreply.github.com>
2023-02-13 19:33:42 -0300

    att js

---
+++ condicoes/ex008.js
@@ -0,0 +1,8 @@
+var vel = 55.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+if (vel >= 60) { //Condição simples!
+    console.log('Você passou do limite da via, Multado!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
