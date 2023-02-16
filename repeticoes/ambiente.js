0adde32f91e4238c7eb7076bde1c323ac0aa3c76
Bruno Schneider <84298233+BrunoSchnei@users.noreply.github.com>
2023-02-16 19:32:56 -0300

    repeticoes att

---
+++ repeticoes/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+do {        // Faça --- Enquanto 
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6 ) 
+
+
+
+var c = 14
+while (c <= 20 ) { // Enquanto --- Faça
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
